198745339407a351268fe1382a86e825254aa501
mehedi-bjit <hasan.mehedi@bjitgroup.com>
2021-07-01 12:19:48 +0600

    created github actions workflow for production stage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Development from "./development";
+import Production from "./Production";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Development/>
+          <Production/>
       </header>
     </div>
   );
